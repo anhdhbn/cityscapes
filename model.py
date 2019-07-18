2ae43eb2713b3857aea4158065b00680b4365ad0
AnhDH <anhdhbn@gmail.com>
2019-07-19 02:03:11 +0700

    init

--- model.py
+++ model.py
@@ -74,3 +74,14 @@ def conv2d_block(input_tensor, n_filters, kernel_size=3, batchnorm=True):
         x = BatchNormalization()(x)
     x = Activation("relu")(x)
     return x
+
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
