74914e33d88c1118bb5ba2d83a5c7edd570ff3eb
AnhDH <anhdhbn@gmail.com>
2019-07-19 09:13:25 +0700

    init

--- model.py
+++ model.py
@@ -100,81 +100,81 @@ def dice_coef(y_true, y_pred):
     return (2. * K.sum(y_true * y_pred) + 1.) / (K.sum(y_true) + K.sum(y_pred) + 1.)
 
 
-def fcn_8s(num_classes, input_shape, lr_init, lr_decay):
+def fcn_8s(num_classes, input_shape, lr_init, lr_decay, n_filters=32):
     img_input = Input(input_shape)
 
     # Block 1
-    x = Conv2D(64, (3, 3), padding='same', name='block1_conv1')(img_input)
+    x = Conv2D(n_filters, (3, 3), padding='same', name='block1_conv1')(img_input)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(64, (3, 3), padding='same', name='block1_conv2')(x)
+    x = Conv2D(n_filters, (3, 3), padding='same', name='block1_conv2')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     x = MaxPooling2D()(x)
 
     # Block 2
-    x = Conv2D(128, (3, 3), padding='same', name='block2_conv1')(x)
+    x = Conv2D(n_filters * 2, (3, 3), padding='same', name='block2_conv1')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(128, (3, 3), padding='same', name='block2_conv2')(x)
+    x = Conv2D(n_filters * 2, (3, 3), padding='same', name='block2_conv2')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     x = MaxPooling2D()(x)
 
     # Block 3
-    x = Conv2D(256, (3, 3), padding='same', name='block3_conv1')(x)
+    x = Conv2D(n_filters * 4, (3, 3), padding='same', name='block3_conv1')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(256, (3, 3), padding='same', name='block3_conv2')(x)
+    x = Conv2D(n_filters * 4, (3, 3), padding='same', name='block3_conv2')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(256, (3, 3), padding='same', name='block3_conv3')(x)
+    x = Conv2D(n_filters * 4, (3, 3), padding='same', name='block3_conv3')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     block_3_out = MaxPooling2D()(x)
 
     # Block 4
-    x = Conv2D(512, (3, 3), padding='same', name='block4_conv1')(block_3_out)
+    x = Conv2D(n_filters * 8, (3, 3), padding='same', name='block4_conv1')(block_3_out)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(512, (3, 3), padding='same', name='block4_conv2')(x)
+    x = Conv2D(n_filters * 8, (3, 3), padding='same', name='block4_conv2')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(512, (3, 3), padding='same', name='block4_conv3')(x)
+    x = Conv2D(n_filters * 8, (3, 3), padding='same', name='block4_conv3')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     block_4_out = MaxPooling2D()(x)
 
     # Block 5
-    x = Conv2D(512, (3, 3), padding='same', name='block5_conv1')(block_4_out)
+    x = Conv2D(n_filters * 8, (3, 3), padding='same', name='block5_conv1')(block_4_out)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(512, (3, 3), padding='same', name='block5_conv2')(x)
+    x = Conv2D(n_filters * 8, (3, 3), padding='same', name='block5_conv2')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
-    x = Conv2D(512, (3, 3), padding='same', name='block5_conv3')(x)
+    x = Conv2D(n_filters * 8, (3, 3), padding='same', name='block5_conv3')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
     x = MaxPooling2D()(x)
 
     # Convolutinalized fully connected layer.
-    x = Conv2D(4096, (7, 7), activation='relu', padding='same')(x)
+    x = Conv2D(n_filters * 64, (7, 7), activation='relu', padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
-    x = Conv2D(4096, (1, 1), activation='relu', padding='same')(x)
+    x = Conv2D(n_filters * 64, (1, 1), activation='relu', padding='same')(x)
     x = BatchNormalization()(x)
     x = Activation('relu')(x)
 
